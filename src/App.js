9b07775f2fcdb8c6b51e8b80641a96181abae279
Daniele Guido <gui.daniele@gmail.com>
2021-03-21 13:07:57 +0100

    add animated logo, in svgm

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './routes'
 import Footer from './components/Footer'
+import Logo from './components/Logo'
+
 import { initializeI18next } from './logic/language'
 
 const { languageCode } = initializeI18next()
@@ -8,6 +10,7 @@ const { languageCode } = initializeI18next()
 const App = () => {
   return (
     <BrowserRouter>
+    <Logo id="logo"/>
     <AppRoutes startLanguageCode={languageCode}/>
     <Footer />
     </BrowserRouter>
