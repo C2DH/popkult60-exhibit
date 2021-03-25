d0c03680cd7ad382ef2d912229d308b2d5b56d0e
Daniele Guido <gui.daniele@gmail.com>
2021-03-25 11:50:24 +0100

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
 import { useCurrentWindowDimensions } from '../hooks'
 import HomeRoom from '../components/HomeRoom'
 import HomeThemes from '../components/HomeThemes'
+import { useStore } from '../store'
+import { useCachedStories } from 'react-miller'
 
+
+let c = 0
 
 const Home = () => {
-  const { t } = useTranslation()
+  const { t, i18n } = useTranslation()
   const { width, height } = useCurrentWindowDimensions()
-
+  const [stories, pagination, { error }] = useCachedStories({
+    filters: {tags__slug: 'theme' }
+  }, {
+    language: i18n.language.split('-').join('_'),
+    defaultLanguage: i18n.options.defaultLocale,
+  })
+  useEffect(() => {
+    useStore.setState({
+      backgroundColor: 'transparent',
+      logoReduced: false 
+    });
+  }, [])
+  c = c+1
+  if(c > 10) {
+   throw new Error('stop')
+  }
+  console.info("Home", stories, pagination, error)
   return (
     <div className="Home">
     <HomeRoom width={width} height={height} />
