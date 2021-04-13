df69e6a52bf7ea91534001209d3a51c8dd5c5394
Daniele Guido <gui.daniele@gmail.com>
2021-04-13 10:23:02 +0200

    remove footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './routes'
-import Footer from './components/Footer'
-import Logo from './components/Logo'
+// import Footer from './components/Footer'
+import Header from './components/Header'
+// import Logo from './components/Logo'
 import MainBackground from './components/MainBackground'
 
 import { initializeI18next } from './logic/language'
@@ -11,10 +12,10 @@ const { languageCode } = initializeI18next()
 const App = () => {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL || ''}>
+      <Header />
       <MainBackground />
-      <Logo id="logo" />
       <AppRoutes startLanguageCode={languageCode} />
-      <Footer />
+
     </BrowserRouter>
   );
 }
