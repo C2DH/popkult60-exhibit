d62d0f9063dfb3e8f9a7cd8c0157e475c35e0d29
Daniele Guido <gui.daniele@gmail.com>
2021-04-19 13:47:06 +0200

    add theme basic info always available (no waiting time for links)

--- src/constants.js
+++ src/constants.js
@@ -14,3 +14,11 @@ export const LanguageCodes = Languages.map((l) => l.split('-')[0])
 export const LanguageRoutePattern = `/:lang(${LanguageCodes.join('|')})`
 export const DefaultLanguage = process.env.REACT_APP_DEFAULT_LANGUAGE ?? 'en-GB'
 export const DefaultLanguageCode = DefaultLanguage.split('-')[0]
+
+export const ThemeRadio = { slug: 'radio-luxembourg' }
+export const ThemePlatine = { slug: 'le-deserteur-de-boris-vian' }
+export const ThemeMagPilote = { slug: 'the-asterix-phenomenon-changes-in-comics-forma' }
+export const ThemePoster = { slug: 'poster' }
+export const ThemeRetro = { slug: 'retro' }
+export const ThemeTv = { slug: 'akk-theme' }
+export const ThemeMagTv = { slug: 'magTv' }
