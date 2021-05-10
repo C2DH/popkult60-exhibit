d89b3553ee193a48c33fe01db5b68b218cda1f15
Daniele Guido <gui.daniele@gmail.com>
2021-05-10 22:23:54 +0200

    Update AppRouteLoading.js

--- src/pages/AppRouteLoading.js
+++ src/pages/AppRouteLoading.js
@@ -4,13 +4,12 @@ import { useTranslation } from 'react-i18next'
 
 const AppRouteLoading = () => {
   const { t } = useTranslation()
-
+  
   return (
     <Container className="AppRouteLoading h-100 text-center">
       <Row className="h-100 d-flex align-items-center">
         <Col>
           <h1 className="my-5">{t('pagesAppRouteLoadingTitle')}</h1>
-          <p>{t('pagesAppRouteLoadingSubheading')}</p>
         </Col>
       </Row>
     </Container>
