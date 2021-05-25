e238c6cc059b6196bde18b4a0102974ddedbca0c
Daniele Guido <gui.daniele@gmail.com>
2021-05-25 12:24:30 +0200

    Update NotFound.js

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
+import { useStore } from '../store'
 
-const NotFound = () => {
+const NotFound = ({ height }) => {
   const { t } = useTranslation()
+  const {changeTheme} = useStore(state => state)
+  useEffect(() => {
+    changeTheme({
+      name: 'themeHome',
+    });
+  }, [changeTheme])
+
   return (
-    <Container style={{ marginTop: 200 }}>
+    <Container className="NotFound" style={{ paddingTop: 200, minHeight: height - 200}}>
       <Row>
         <Col>
           <h1>{t('pagesNotFoundTitle')}</h1>
