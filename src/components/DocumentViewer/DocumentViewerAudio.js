85260096852c333cd8b17a9538ba04bfdd245a45
Daniele Guido <gui.daniele@gmail.com>
2021-05-26 16:55:13 +0200

    Update DocumentViewerAudio.js

--- src/components/DocumentViewer/DocumentViewerAudio.js
+++ src/components/DocumentViewer/DocumentViewerAudio.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Video from '../Video'
-// import { get } from 'lodash'
+import { get } from 'lodash'
 
 const DocumentViewerAudio = ({ doc, height, paddingTop=100}) => {
   const videoUrl = doc?.data?.streamingUrl
@@ -8,11 +8,17 @@ const DocumentViewerAudio = ({ doc, height, paddingTop=100}) => {
     console.error('Video not valid, doc data must contain a streamingUrl')
     return null
   }
-  // const backgroundImage = get(doc, 'data.resolutions.preview.url', false)
+  const backgroundImage = get(doc, 'data.resolutions.preview.url', false)
   const availableHeight = 100
-  const controlsAvailableHeight = 100
+  const controlsAvailableHeight = 50
   return (
     <div className="DocumentViewerAudio" style={{ minHeight: availableHeight, paddingTop }}>
+      <div className="px-2 bg-dark">
+        <div style={{backgroundColor: 'var(--dark)',height: controlsAvailableHeight, backgroundImage: `url(${backgroundImage})`,
+            backgroundSize : 'cover',
+            backgroundPosition: 'center'
+        }} />
+      </div>
       <Video
         url={videoUrl}
         width={"auto"}
